62c64ed62a1a2c1b87691c73f77990845e7529ab
Ivan Izaguirre <ivanizag@gmail.com>
2021-11-09 20:01:37 +0100

    Compare the binaries

--- 81-149c/verify.sh
+++ 81-149c/verify.sh
@@ -2,4 +2,5 @@ z80asm -lbios.prn bios.s
 z80dasm -a -l -t a.bin | tail -n+3 > source_assembled_and_disassembled
 z80dasm -a -l -t 81-149c.rom | tail -n+3 > original_bios_disassembled
 echo
+diff a.bin 81-149c.rom -s
 diff original_bios_disassembled source_assembled_and_disassembled -s
