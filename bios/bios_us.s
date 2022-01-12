1b04e8f3cddc0ccc3f24255cde8dab80e62415e0
Ivan Izaguirre <ivanizag@gmail.com>
2022-01-12 23:31:25 +0100

    Analysis of the CP/M BIOS for the US version

---
+++ bios/bios_us.s
@@ -0,0 +1,357 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; CONSTANTS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+io_00_serial_baud_rate:     EQU 0x00
+io_1c_system_bits:          EQU 0x1c
+system_bit_motors_neg:        EQU 6
+system_bit_bank:              EQU 7
+
+
+reset:                      EQU 0x0000
+iobyte:                     EQU 0x0003
+user_drive:                 EQU 0x0004
+bdos_ep:                    EQU 0x0005
+bdos_boot:                  EQU 0xe400
+bdos_entrypoint:            EQU 0xec06
+rom_stack:                  EQU 0xfc00
+disk_DMA_address:           EQU 0xfc14 ; 2 bytes
+ram_e407:                   EQU 0xe407
+
+; See CP/M 2.2 System alteration guide appendix G
+rw_type_directory_write:    EQU 1
+
+ROM_INITDSK:   EQU 0x03
+ROM_HOME:      EQU 0x0c
+ROM_SELDSK:    EQU 0x0f
+ROM_SETTRK:    EQU 0x12
+ROM_SETSEC:    EQU 0x15
+ROM_SETDMA:    EQU 0x18
+ROM_READ:      EQU 0x1b
+ROM_WRITE:     EQU 0x1e
+ROM_SECTRAN:   EQU 0x21
+ROM_KBDSTAT:   EQU 0x2a
+ROM_KBDIN:     EQU 0x2d
+ROM_SIOSTI:    EQU 0x33
+ROM_SIOIN:     EQU 0x36
+ROM_SIOOUT:    EQU 0x39
+ROM_LISTST:    EQU 0x3c
+ROM_LIST:      EQU 0x3f
+ROM_SERSTO:    EQU 0x42
+ROM_VIDOUT:    EQU 0x45
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; BIOS ENTRY POINTS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+ORG 0fa00h
+    JP BOOT
+EP_WBOOT:
+    JP WBOOT
+    JP CONST
+    JP CONIN
+    JP CONOUT
+    JP LIST
+    JP PUNCH
+    JP READER
+    JP HOME
+    JP SELDSK
+    JP SETTRK
+    JP SETSEC
+    JP SETDMA
+    JP READ
+    JP WRITE
+    JP LISTST
+    JP SECTRAN
+
+iobyte_default:
+    DB 81h ; Console to CRT and List to parallel port
+bios_config:
+    DB 00h
+control_char_conversion:
+    DB 0Bh, 0Ah, 08h, 0Ch
+    DB "0123"
+    DB "4567"
+    DB "89-,"
+    DB "\r."
+baud_rate_default:
+    DB 05h
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; BOOT
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+BOOT:
+    CALL INITDSK
+    ; User 0, drive 0
+    XOR A
+    LD (user_drive), A
+    ; Reset iobyte
+    LD A, (iobyte_default)
+    LD (iobyte), A
+    ; Reset serial baur rate
+    LD A, (baud_rate_default)
+    OUT (io_00_serial_baud_rate), A
+    CALL WRITE_STRING_INLINE
+    DB 1Ah,"\r\nKAYPRO II 64k CP/M vers 2.2\r\n",0
+BOOT_SILENT:
+    LD A,0xc3 ; JP opcode
+    ; Set the WBOOT jump at address 0
+    LD HL, EP_WBOOT
+    LD (reset), A
+    LD (reset+1), HL
+    ; Set BDOS entry point at address 5
+    LD HL, bdos_entrypoint
+    LD (bdos_ep), A
+    LD (bdos_ep+1), HL
+    ; Continue boot to CCP
+    LD A, (user_drive)
+    LD C, A
+    JP bdos_boot
+
+WBOOT:
+    CALL INITDSK
+    CALL WRITE_STRING_INLINE
+    DB "\r\nWarm Boot\r\n",0
+
+WBOOT_SILENT:
+    ; Rest the stack
+    LD SP,0x100
+    ; Select drive 0, track 0
+    LD C,0x0
+    CALL SELDSK
+    LD BC,0x0
+    CALL SETTRK
+    ; Reset default dma address
+    LD HL,bdos_boot
+    LD (disk_DMA_address),HL
+    LD BC,0x2c01
+WBOOT_LOOP:
+    PUSH BC
+    CALL SETSEC
+    CALL READ
+    POP BC
+    OR A
+    JR NZ,WBOOT_SILENT
+    LD HL,(disk_DMA_address)
+    LD DE,0x80
+    ADD HL,DE
+    LD (disk_DMA_address),HL
+    XOR A
+    LD (ram_e407),A
+    DEC B
+    JP Z,BOOT_SILENT
+    INC C
+    LD A,0x28
+    CP C
+    JP NZ,WBOOT_LOOP
+    LD C,0x10
+    PUSH BC
+    LD C,0x1
+    CALL SETTRK
+    POP BC
+    JR WBOOT_LOOP
+
+CONST:
+    ; Make sure the disk is off once every 256 calls
+    LD HL,CONST_COUNTER
+    INC (HL)
+    CALL Z,DISKOFF
+    ;
+    LD A,(iobyte)
+    AND 0x3
+    LD L,ROM_SIOSTI
+    JP Z,ROM_JUMP
+    LD L,ROM_KBDSTAT
+    JP ROM_JUMP
+
+CONIN:
+    CALL DISKOFF
+    LD A,(iobyte)
+    AND 0x3
+    LD L,ROM_SIOIN
+    JP Z,ROM_JUMP
+    LD L,ROM_KBDIN
+    CALL ROM_JUMP
+    OR A
+    RET P
+    AND 0x1f ; control char conversion table is smaller than 32!!
+    LD HL, control_char_conversion
+    LD C,A
+    LD B,0x0
+    ADD HL,BC
+    LD A,(HL)
+    RET
+
+DISKOFF:
+    IN A, (io_1c_system_bits)
+    SET system_bit_motors_neg, A
+    OUT (io_1c_system_bits), A
+    RET
+
+CONOUT:
+    LD A,(iobyte)
+    AND 0x3
+    LD L,ROM_SIOOUT
+    JP Z,ROM_JUMP
+    LD L,ROM_VIDOUT
+    JP ROM_JUMP
+
+READER:
+    LD L,ROM_SIOIN
+    JP ROM_JUMP
+
+PUNCH:
+    LD L,ROM_SIOOUT
+    JP ROM_JUMP
+
+LIST:
+    LD A,(iobyte)
+    AND 0xc0
+    LD L,ROM_SIOOUT
+    JP Z,ROM_JUMP
+    LD L,ROM_LIST
+    CP 0x80
+    JP Z,ROM_JUMP
+    LD L,ROM_VIDOUT
+    CP 0x40
+    JP Z,ROM_JUMP
+    LD L,ROM_SIOOUT
+    JP ROM_JUMP
+
+LISTST:
+    LD A,(iobyte)
+    AND 0xc0
+    LD L,ROM_SERSTO
+    JP Z,ROM_JUMP
+    LD L,ROM_LISTST
+    CP 0x80
+    JP Z,ROM_JUMP
+    XOR A
+    RET
+
+INITDSK:
+    LD L,ROM_INITDSK
+    JR ROM_JUMP
+
+HOME:
+    LD L,ROM_HOME
+    JR ROM_JUMP
+
+SELDSK:
+    LD L,ROM_SELDSK
+    JR ROM_JUMP
+
+SETTRK:
+    LD L,ROM_SETTRK
+    JR ROM_JUMP
+
+SETSEC:
+    LD L,ROM_SETSEC
+    JR ROM_JUMP
+
+SETDMA:
+    LD L,ROM_SETDMA
+    JR ROM_JUMP
+
+READ:
+    ; Reset the CONST counter
+    XOR A
+    LD (CONST_COUNTER), A
+    ; Read
+    LD L,ROM_READ
+    JR ROM_JUMP
+
+WRITE:
+    ; Reset the CONST counter
+    XOR A
+    LD (CONST_COUNTER), A
+    ; Write
+    LD L,ROM_WRITE
+    ; Is the bios_config 0?
+    LD A,(bios_config)
+    OR A
+    ; Yes, write normally
+    JR Z,ROM_JUMP
+    ; No, force the write type
+    LD C,rw_type_directory_write
+    JR ROM_JUMP
+
+SECTRAN:
+    LD L,ROM_SECTRAN
+    JR ROM_JUMP
+
+ROM_JUMP:
+    EXX
+    ; Activate the ROM bank
+    IN A, (io_1c_system_bits)
+    SET system_bit_bank, A
+    OUT (io_1c_system_bits), A
+    ; Switch to the ROM stack
+    LD (STACK_SAVE), SP
+    LD SP, rom_stack
+    ; Push the ROM cleanup code to RET there
+    LD DE, ROM_JUMP_CLEANUP
+    PUSH DE
+    EXX
+    ; Jump to the ROM address in L, page 0
+    LD H,0x0
+    JP (HL)
+
+ROM_JUMP_CLEANUP:
+    EX AF, AF' ;'
+    ; Restore the stack
+    LD SP, (STACK_SAVE)
+    ; Deactivate the ROM bank
+    IN A, (io_1c_system_bits)
+    RES system_bit_bank, A
+    OUT (io_1c_system_bits), A
+    EX AF, AF' ; '
+    ; Return to the caller
+    RET
+
+WRITE_STRING_INLINE:
+    ; Retrieve the char at the return address
+    EX (SP), HL
+    LD A, (HL)
+    ; Increase the return address
+    INC HL
+    EX (SP), HL
+    ; Is the char a null?
+    OR A
+    ; Yes, return
+    RET Z
+    ; No, write it
+    LD C, A
+    CALL CONOUT
+    ; Process the next char
+    JR WRITE_STRING_INLINE
+
+CONST_COUNTER:
+    DB $04
+STACK_SAVE:
+    DW $EF35
+FILLER:
+    DS 14, $00
+    DB $8E
+    DB $04
+    DB $8E
+    DB $04
+    DB $03
+    DB $04
+    DB $88
+    DB $9C
+    DB $C5
+    DB $04
+    DB $53
+    DB $FF
+    DB $14
+    DB $05
+    DB $0F
+    DB $04
+    DB $13
+    DB $06
+    DW ROM_JUMP_CLEANUP
+
