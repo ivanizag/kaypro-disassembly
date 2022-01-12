0b76c8f92622d8f8d0558bf0400c174c679c2594
Ivan Izaguirre <ivanizag@gmail.com>
2022-01-12 23:38:56 +0100

    Organise ROM files

---
+++ rom/verify.sh
@@ -0,0 +1,7 @@
+name=$(basename "$1" .s)
+z80asm -l$name.prn $name.s
+z80dasm -a -l -t a.bin | tail -n+3 > source_assembled_and_disassembled
+z80dasm -a -l -t $name.rom | tail -n+3 > original_disassembled
+echo
+diff a.bin $name.rom -s
+diff original_disassembled source_assembled_and_disassembled -s
